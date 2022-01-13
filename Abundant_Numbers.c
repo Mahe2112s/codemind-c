ce0bb69c7a0bcfb52597bceb54c5eaedd51c683b
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-01-13 10:34:03 +0530

    #include<stdio.h> int main() {     int m,n,s;     scanf("%d",&m);     for(n=1;n<m;n++)     {         if(m%n==0)         s=s+n;     }     if(s>m)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,s;
+    scanf("%d",&m);
+    for(n=1;n<m;n++)
+    {
+        if(m%n==0)
+        s=s+n;
+    }
+    if(s>m)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
