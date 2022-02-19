761771e44da245e79e3326f106a5c42bfb6b9b6e
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-02-19 07:41:33 +0530

    #include<stdio.h> int main() {     int s[100],n,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&s[i]);     }     for(i=0;i<n;i++)     {         if(s[i]%2!=0)         {             if(i%2==0)             {                 printf("False");                 return 0;             }         }     }     printf("True"); }

---
+++ Strictly_ODD.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int s[100],n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&s[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(s[i]%2!=0)
+        {
+            if(i%2==0)
+            {
+                printf("False");
+                return 0;
+            }
+        }
+    }
+    printf("True");
+}
