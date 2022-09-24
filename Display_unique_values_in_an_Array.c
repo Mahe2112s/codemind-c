675aa57cc28bb9e11c23fc0e3ca2d950ae29f5a8
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2022-09-24 14:32:55 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int q[100];     int l=0,j;     q[l]=arr[0];     for(i=0;i<n;i++)     {         int  m=0;         for(j=0;j<l;j++)         {             if(q[j]==arr[i])             {                 m++;             }         }          if(m==0)         {            q[l++]=arr[i];          }     }     int f=0;     for(j=0;j<l;j++)     {        int c=0;        for(i=0;i<n;i++)         {            if(q[j]==arr[i])            {                c++;            }         }         if(c==1)         {             printf("%d ",q[j]);             f=1;         }     }     if(f==0)     {         printf("-1");     } }

---
+++ Display_unique_values_in_an_Array.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int q[100];
+    int l=0,j;
+    q[l]=arr[0];
+    for(i=0;i<n;i++)
+    {
+        int  m=0;
+        for(j=0;j<l;j++)
+        {
+            if(q[j]==arr[i])
+            {
+                m++;
+            }
+        }
+         if(m==0)
+        {
+           q[l++]=arr[i];     
+         }
+    }
+    int f=0;
+    for(j=0;j<l;j++)
+    {
+       int c=0;
+       for(i=0;i<n;i++)
+        {
+           if(q[j]==arr[i])
+           {
+               c++;
+           }
+        }
+        if(c==1)
+        {
+            printf("%d ",q[j]);
+            f=1;
+        }
+    }
+    if(f==0)
+    {
+        printf("-1");
+    }
+}
